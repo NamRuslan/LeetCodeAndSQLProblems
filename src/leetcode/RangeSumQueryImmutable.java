754fd7273c392dd6ad7f361930756ac4a68c136e
NamRuslan <rus.nam.96@bk.ru>
2021-07-01 14:21:32 +0300

    303. Range Sum Query - Immutable

---
+++ src/leetcode/RangeSumQueryImmutable.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * Given an integer array nums, handle multiple queries of the following type:
+ *
+ * Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+ * Implement the NumArray class:
+ *
+ * NumArray(int[] nums) Initializes the object with the integer array nums.
+ * int sumRange(int left, int right) Returns the sum of the elements of nums
+ * between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ */
+public class RangeSumQueryImmutable {
+    class NumArray {
+
+        int[] sums;
+
+        public NumArray(int[] nums) {
+
+            sums = new int[nums.length + 1];
+            for (int i = 0; i < nums.length; i++) {
+                sums[i + 1] = sums[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            return sums[right + 1] - sums[left];
+        }
+    }
+}
